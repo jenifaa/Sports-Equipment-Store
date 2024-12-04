7c29214d02f411cf808a64d267be176432f4f3fd
jenifaa <jefrahman32@gmail.com>
2024-12-04 22:26:52 +0600

    first commit

---
+++ src/component/Main/MainLayOut.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Pages/Navbar";
+
+const MainLayOut = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainLayOut;
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayOut from './component/Main/MainLayOut.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayOut></MainLayOut>
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
